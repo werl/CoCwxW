8b849a3e8801fa6242092f7299de52dd9c7c0064
Peter L <peter@werl.me>
2021-12-24 14:44:17 -0700

    Test

---
+++ src/panels/coc_view_panel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by peter on 2021-11-25.
+//
+
+#include "coc_view_panel.hpp"
---
+++ src/panels/coc_view_panel.hpp
@@ -0,0 +1,14 @@
+//
+// Created by peter on 2021-11-25.
+//
+
+#ifndef COCWXW_COC_VIEW_PANEL_HPP
+#define COCWXW_COC_VIEW_PANEL_HPP
+
+
+class COCViewPanel {
+
+};
+
+
+#endif //COCWXW_COC_VIEW_PANEL_HPP
